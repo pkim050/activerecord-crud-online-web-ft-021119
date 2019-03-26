dd8f3a474507a443d16d27cd4101447d9b7852b1
Patrick Kim <pkim050@gmail.com>
2019-03-26 14:51:46 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -44,7 +44,8 @@ def can_get_the_last_item_in_the_database
 end
 
 def can_get_size_of_the_database
-  Movie.new.size
+  temp = Movie.new
+  temp.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
