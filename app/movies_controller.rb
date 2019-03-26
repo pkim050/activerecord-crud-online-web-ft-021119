f6f421842a2c9b7f8c83627679cadfeea7bc1576
Patrick Kim <pkim050@gmail.com>
2019-03-26 14:47:31 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -36,7 +36,8 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  sql = "SELECT * FROM movies WHERE id = 1"
+  DB[:conn].execute(sql)
 end
 
 def can_get_the_last_item_in_the_database
